8ffd2deb76a0f264c248a55fe05e9407c53a64f0
df329 <df329@hotmail.com>
2020-01-01 18:27:00 +1100

    Basic chicken feather picker

---
+++ src/Machiavellianism/task/Task.java
@@ -0,0 +1,28 @@
+package Machiavellianism.task;
+
+import org.powerbot.script.Area;
+import org.powerbot.script.ClientAccessor;
+import org.powerbot.script.ClientContext;
+
+/**
+ * Task class for defining an action that will be conditionally (activate) executed.
+ * @param <C> Client context
+ */
+public abstract class Task<C extends ClientContext> extends ClientAccessor<C> {
+    public Task(C ctx) {
+        super(ctx);
+    }
+
+    /**
+     * Whether to execute the action.
+     * @return true to execute the action else false
+     */
+    public abstract boolean activate();
+
+    /**
+     * Execute the action in the area.
+     * @param area the area to execute the action
+     * @return if greater than zero, the action was successful
+     */
+    public abstract int execute(Area area);
+}
